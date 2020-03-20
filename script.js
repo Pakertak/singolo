2556f9d662b15ae6f55db1947a898ae91b2766a0
Valiantsin Horbach <valentin.com2000gorbach@gmail.com>
2020-03-20 14:34:44 +0300

    add: form reset after submit

--- script.js
+++ script.js
@@ -13,19 +13,23 @@ const BUTTON_LEFT = document.getElementById('left');
 const BUTTON_RIGHT = document.getElementById('right');
 
 
-HEADER_NAV.addEventListener('click', (event)=>{
+HEADER_NAV.addEventListener('click', (event) => {
+
     HEADER_NAV.querySelectorAll('li').forEach(el=>el.classList.remove('navigation--item-selected'));
-    if(event.target!=HEADER_NAV) event.target.parentNode.classList.add('navigation--item-selected');
-})
 
-PORTFOLIO_MENU.addEventListener('click', (event)=>{
+    if(event.target!=HEADER_NAV) 
+        event.target.parentNode.classList.add('navigation--item-selected');
+
+});
+
+PORTFOLIO_MENU.addEventListener('click', (event) => {
+
     PORTFOLIO_MENU.querySelectorAll('li').forEach(el=>el.classList.remove('nav-item--selected'));
-    if(event.target!=PORTFOLIO_MENU) event.target.parentNode.classList.add('nav-item--selected');
-})
 
-BUTTON.addEventListener('click', ()=>{
-    
-})
+    if(event.target!=PORTFOLIO_MENU) 
+        event.target.parentNode.classList.add('nav-item--selected');
+});
+
 
 BUTTON.addEventListener("click", (event) => {
     let requiredFields = [...document.querySelectorAll("[required]")];
@@ -33,6 +37,7 @@ BUTTON.addEventListener("click", (event) => {
   
     //Check if all required fields filled with valid data
     if ( requiredFields.every(isValid) ) {
+
       event.preventDefault();
       const subject = document.getElementById('subject').value.toString();
         if(subject!='') document.getElementById('resultSubject').innerText = ("Тема: " + subject);
@@ -44,80 +49,103 @@ BUTTON.addEventListener("click", (event) => {
     }
   });
 
-CLOSE_BUTTON.addEventListener('click', ()=>{
+CLOSE_BUTTON.addEventListener('click', () => {
+    document.getElementById('form').reset();
     document.getElementById('resultSubject').innerText = '';
     document.getElementById('resultDescription').innerText = '';
     document.getElementById('messageBlock').classList.add('hidden');
-})
 
-IMGS_CONTAINER.addEventListener('click', (event)=>{
-    IMGS_CONTAINER.querySelectorAll('div').forEach(el=>el.classList.remove('img--selected'));
-    if(event.target!=IMGS_CONTAINER) event.target.classList.add('img--selected');
-})
+});
+
+IMGS_CONTAINER.addEventListener('click', (event) => {
+
+    IMGS_CONTAINER.querySelectorAll('div').forEach(el => el.classList.remove('img--selected'));
+    if( event.target!=IMGS_CONTAINER ) event.target.classList.add('img--selected');
+
+});
  
-PORTFOLIO_ALL.addEventListener('click', ()=>{
-    IMGS_CONTAINER.querySelectorAll('div').forEach((element, i)=>{
+PORTFOLIO_ALL.addEventListener('click', () => { 
+    IMGS_CONTAINER.querySelectorAll('div').forEach((element, i) => {
         for(let j = 0; j < 11; j++)
         element.classList.remove('img' + j);
         element.classList.add('img' + i);
-   }) 
-})
+   });
+});
 
-PORTFOLIO_WEB_DESIGN.addEventListener('click', ()=>{
-    IMGS_CONTAINER.querySelectorAll('div').forEach((element, i)=>{
+PORTFOLIO_WEB_DESIGN.addEventListener('click', () => {
+    IMGS_CONTAINER.querySelectorAll('div').forEach((element, i) => {
         for(let j = 0; j < 11; j++)
         element.classList.remove('img' + j);
         element.classList.remove('img' + i);
         element.classList.add('img' + Math.floor(Math.random() * Math.floor(11)));
-   }) 
-})
+   }); 
+});
 
-PORTFOLIO_GRAPHIC_DESIGN.addEventListener('click', ()=>{
+PORTFOLIO_GRAPHIC_DESIGN.addEventListener('click', () => {
     IMGS_CONTAINER.querySelectorAll('div').forEach((element, i)=>{
+
         for(let j = 0; j < 11; j++)
         element.classList.remove('img' + j);
+
         element.classList.remove('img' + i);
         element.classList.add('img' + Math.floor(Math.random() * Math.floor(11)));
-   }) 
-})
+
+   });
+});
 
 PORTFOLIO_ARTWORK.addEventListener('click', ()=>{
     IMGS_CONTAINER.querySelectorAll('div').forEach((element, i)=>{
+
         for(let j = 0; j < 11; j++)
         element.classList.remove('img' + j);
+
         element.classList.remove('img' + i);
         element.classList.add('img' + Math.floor(Math.random() * Math.floor(11)));
-   }) 
-})
+
+   }); 
+});
 
 BUTTON_VERTICAL.addEventListener('click', ()=>{
-    if(!document.getElementById('windowVertical').classList.contains('windowOff')) document.getElementById('windowVertical').classList.add('windowOff');
-    else document.getElementById('windowVertical').classList.remove('windowOff');
-})
+
+    if(!document.getElementById('windowVertical').classList.contains('windowOff')) 
+        document.getElementById('windowVertical').classList.add('windowOff');
+
+    else 
+        document.getElementById('windowVertical').classList.remove('windowOff');
+});
 
 BUTTON_HORIZONTAL.addEventListener('click', ()=>{
-    if(!document.getElementById('windowHorizontal').classList.contains('windowOff')) document.getElementById('windowHorizontal').classList.add('windowOff');
-    else document.getElementById('windowHorizontal').classList.remove('windowOff');
-})
+
+    if(!document.getElementById('windowHorizontal').classList.contains('windowOff')) 
+        document.getElementById('windowHorizontal').classList.add('windowOff');
+
+    else 
+        document.getElementById('windowHorizontal').classList.remove('windowOff');
+});
 
 BUTTON_LEFT.addEventListener('click', ()=>{
+
     if(document.getElementById('slider1').classList.contains('hidden')) { 
         document.getElementById('slider2').classList.add('hidden'); 
         document.getElementById('slider1').classList.remove('hidden');
     }
+    
     else {
         document.getElementById('slider2').classList.remove('hidden');
         document.getElementById('slider1').classList.add('hidden');
     }
-})
+});
 
 BUTTON_RIGHT.addEventListener('click', ()=>{
+
     if(document.getElementById('slider1').classList.contains('hidden')) { 
         document.getElementById('slider2').classList.add('hidden'); 
         document.getElementById('slider1').classList.remove('hidden');
     }
+
     else {
         document.getElementById('slider2').classList.remove('hidden');
         document.getElementById('slider1').classList.add('hidden');
     }
-})
+
+});
